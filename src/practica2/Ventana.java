d8383e4615584d6116f74930cc992fc2c3f06f26
Alejo972017Marin <alejandrograndemarin@hotmail.com>
2017-10-12 23:38:10 -0600

    Version.1

--- src/practica2/Ventana.java
+++ src/practica2/Ventana.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
 import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -64,47 +65,64 @@ public class Ventana extends JFrame {
         vel.reshape(10, 100, 70, 20);
         Pausa.reshape(10, 150, 100, 30);
         Btn1();
-        llenar();
+       
         Pausa.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent evt) {
                 PausaActionPerformed(evt);
             }
-
+int conti=0;
+int contj=0;
             private void PausaActionPerformed(ActionEvent evt) {
-                //Hilo h = new Hilo();
-                //Pausa.setEnabled(false);
-                //h.start();
-                for (int i = 0; i < botones.length; i++) {
-                    for (int j = 0; j < botones[i].length; j++) {
-                        if (botones[i][j].getText().equals(".")) {
-                            for (int k = j-1; k < j+2; k++) {
-                                if (botones[i-1][k].getText().equals("")||botones[j-1][k].getText().equals("")) {
-                                    botones[i-1][k].setBackground(Color.DARK_GRAY);
-                                    botones[i-1][k].setText("");
-                                    botones[k][j-1].setBackground(Color.BLACK);
-                                    botones[k][j-1].setText("");
-                                }
-                            }
+                Hilo h = new Hilo();
+                Pausa.setEnabled(false);
+                h.start();
+               /*
+                for (int i = 1; i < botones.length-2; i++) {
+                    for (int j = 1; j < botones[i].length-1; j++) {
+                        //Lectura Esquinas donde 9=n-1
+                        if (botones[0][0].getText().equals(".")||botones[0][9].getText().equals(".")||botones[9][0].getText().equals(".")||botones[9][9].getText().equals(".")) {
+                        if (botones[0][0].getText().equals(".")) {botones[0][1].setText("X"); botones[1][0].setText("X"); botones[1][1].setText("X");}   
+                        if (botones[0][9].getText().equals(".")) {botones[0][8].setText("X"); botones[1][8].setText("X"); botones[1][9].setText("X");} 
+                        if (botones[9][0].getText().equals(".")) {botones[8][0].setText("X"); botones[8][1].setText("X"); botones[9][1].setText("X");}
+                        if (botones[9][9].getText().equals(".")) {botones[8][8].setText("X"); botones[8][9].setText("X"); botones[9][8].setText("X");}
+                        }else
+                       
+                        //Lectura bordes
+                       if (botones[i][0].getText().equals(".")||botones[i][9].getText().equals(".")||botones[0][j+1].getText().equals(".")||botones[9][j+1].getText().equals(".")) {
                             
-                        }/*else 
-                            if (botones[i][j].getText().equals("")) {
-                                
-                                for (int k = j-1; k < j+2; k++) {
-                                    if (botones[i-1][k].getText().equals(".")||botones[j-1][k].getText().equals(".")) {
-                                        
-                                        botones[i-1][k].setBackground(Color.WHITE);
-                                        botones[i-1][k].setText(".");
-                                        
-                                    }
-                                }
-                                     
-                            
-                        }*/
+                            if (botones[i][0].getText().equals(".")) {botones[i-1][0].setText("X"); botones[i-1][1].setText("X"); botones[i][1].setText("X"); botones[i+1][0].setText("X"); botones[i+1][1].setText("X");}
+                            if (botones[i][9].getText().equals(".")) {botones[i-1][8].setText("Y"); botones[i-1][9].setText("Y"); botones[i][8].setText("Y"); botones[i+1][8].setText("Y"); botones[i+1][9].setText("Y"); }
+                            if (botones[0][j].getText().equals(".")) {botones[0][j-1].setText("Y"); botones[0][j+1].setText("Y"); botones[1][j-1].setText("Y"); botones[1][j].setText("Y"); botones[1][j+1].setText("Y"); }
+                            if (botones[9][j].getText().equals(".")) {botones[8][j-1].setText("X"); botones[8][j].setText("X"); botones[8][j+1].setText("X"); botones[9][j-1].setText("X"); botones[9][j+1].setText("X"); }
+                        }else
+                           //Lectura parte interna 
+                    
+                           
+                       if (botones[i][j].getText().equals(".")) {
+                           
+                       botones[i-1][j].setText("X"); 
+                       botones[i-1][j+1].setText("X"); 
+                       botones[i][j+1].setText("X"); 
+                       botones[i+1][j].setText("X"); 
+                       botones[i+1][j+1].setText("X");
+                       botones[i][j-1].setText("Y"); 
+                       botones[i+1][j-1].setText("Y");
+                       botones[i-1][j-1].setText("Y");
+                       
+                       }
+                      
+                          
+                        
                         
                         
                     }
+                    
                 }
+                
+               */
+                
+                
 
             }
 
@@ -113,7 +131,7 @@ public class Ventana extends JFrame {
     }
 
     public void Btn1() {
-        int n = 5;
+        int n = 10;
         int tam = n;
         botones = new Btn[tam][tam];
         Tablero.setLayout(new GridLayout(tam, tam));
@@ -128,18 +146,46 @@ public class Ventana extends JFrame {
         Tablero.updateUI();
     }
 
-    public void llenar() {
-
-        
-
-    }
-
     private class Hilo extends Thread {
 
         public void run() {
+            
+            while(true){
+                for (int i = 1; i < botones.length-2; i++) {
+                    for (int j = 1; j < botones[i].length-1; j++) {
+                        
+                        //Lectura parte interna
+                        if (botones[i][j].getText().equals(".")) {
+                            if (botones[i-1][j].getText().equals("")) { }
+                          
+                            
+                            //esquina superior izquierda
+                            botones[i-1][j-1].setText("X");
+                            // arriba centro
+                            botones[i-1][j].setText("X");
+                            //esquina superior derecha
+                            botones[i-1][j+1].setText("X");
+                            //medio izquierda
+                            botones[i][j-1].setText("X");
+                            //medio derecha
+                            botones[i][j+1].setText("x");
+                             //esquina Inferior izquierda
+                            botones[i+1][j-1].setText("X");
+                            //abajo centro
+                            botones[i+1][j].setText("X");
+                            //Esquina Inferior derecha
+                            botones[i+1][j+1].setText("X");
+                            
+                           
+                            
+                            
+                        }
+                        
+                    }
+                }
+            }
 
-            botones[3][2].setBackground(Color.yellow);
-            botones[3][2].setText("HOLA");
+
 
         }
     }
